474d7dd86d7df6de61e3059eb532a295c687b481
LeHoser <nathan.lloyd.7003@gmail.com>
2022-10-17 11:59:25 -0300

    Update Program.cs
    Added regenerate shield method
    added level up method
    added gain exp method
    added onto heal method
    added checks into enemy encounter method
    added console colours for health, shields and lives
    added colour to negative effects

--- HealthSystemAPI/Program.cs
+++ HealthSystemAPI/Program.cs
@@ -13,12 +13,16 @@ namespace HealthSystemAPI
         static int playerShield;
         static int playerMana;
         static int playerExperience;
+        static int playerExperienceThreshhold;
         static int playerLevel;
+
+        static public bool impEncounter;
+        static public bool goblinEncounter;
+        static public bool orcEncounter;
+        static public bool dragonEncounter;
 
         static int Rifle;
         static int Shotgun;
-
-        static int enemyHealth;
 
         static int weaponDamage;
 
@@ -33,6 +37,7 @@ namespace HealthSystemAPI
             playerMana = 20;
             playerLives = 3;
             playerExperience = 0;
+            playerExperienceThreshhold = 100;
             playerLevel = 1;
 
             Enemy();
@@ -46,13 +51,18 @@ namespace HealthSystemAPI
 
         static void ShowHUD()
         {
+            Console.WriteLine("===========");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("===========");
             Console.WriteLine("Player HP: " + playerHealth);
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("===========");
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Player Stamina: " + playerShield);
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("===========");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Player Lives: " + playerLives);
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("===========");
         }
 
@@ -63,18 +73,38 @@ namespace HealthSystemAPI
 
             if(enemyEncounter == 1 || enemyEncounter == 2 || enemyEncounter == 3)
             {
+                impEncounter = true;
+                goblinEncounter = false;
+                orcEncounter = false;
+                dragonEncounter = false;
+
                 Console.WriteLine("An Imp has appeared!");
             }
             else if(enemyEncounter == 4 || enemyEncounter == 5 || enemyEncounter == 6)
             {
+                impEncounter = false;
+                goblinEncounter = true;
+                orcEncounter = false;
+                dragonEncounter = false;
+
                 Console.WriteLine("A Goblin has appeared!");
             }
             else if(enemyEncounter == 7 || enemyEncounter == 8 || enemyEncounter == 9)
             {
+                impEncounter = false;
+                goblinEncounter = false;
+                orcEncounter = true;
+                dragonEncounter = false;
+
                 Console.WriteLine("An Orc has appeared!");
             }
             else
             {
+                impEncounter = false;
+                goblinEncounter = false;
+                orcEncounter = false;
+                dragonEncounter = true;
+
                 Console.WriteLine("A Dragon has appeared!");
                 Console.WriteLine("Quick!! Run for your life!");
             }
@@ -82,18 +112,147 @@ namespace HealthSystemAPI
 
         static void HealHP(int healAmount)
         {
-            healAmount = 25;
-            playerMana -= 5;
+            if(healAmount < 0)
+            {
+                Console.WriteLine("The player cannot heal a negative ammount");
+            }
+
+            else
+            {
+                if (playerHealth < 100 && playerMana >= 5)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Player is about to heal " + healAmount + " points of health");
+                    playerMana -= 5;
+                    if (playerHealth > 100)
+                    {
+                        playerHealth = 100;
+                    }
+                }
+                else if (playerHealth == 100 || playerMana < 5)
+                {
+                    Console.WriteLine("Unable to heal!");
+                    if (playerMana < 5)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Not enough mana!");
+                    }
+                    else if (playerHealth == 100)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Player has max health");
+                    }
+                }
+            }
         }
 
         static void GainEXP(int expPoints)
         {
+            int spillOver;
 
+            if(expPoints < 0)
+            {
+                Console.WriteLine("Player cannot take negative experience points");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("The player is gaining" + expPoints + " points of experience");
+                playerExperience = playerExperience + expPoints;
+                if(playerExperience > 100)
+                {
+                    spillOver = playerExperience - 100;
+                    playerExperience = spillOver;
+                }
+            }
         }
 
         static void LevelUp()
         {
+            if (playerExperience >= playerExperienceThreshhold)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("The player is leveling up");
+                playerLevel += 1;
+            }
+        }
 
+        static void TakeDamage(int damage)
+        {
+            int spillOverAmout;
+
+            if(damage < 0)
+            {
+                Console.WriteLine("The player cannot take negative damage!");
+            }
+
+            else
+            {
+                if (playerShield > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Player is about to take " + damage + " points of damage");
+                    playerShield -= damage;
+                    if (playerShield < 0)
+                    {
+                        spillOverAmout = playerShield;
+                        playerHealth = playerHealth - spillOverAmout;
+                        playerShield = 0;
+                        if (playerHealth <= 0)
+                        {
+                            playerLives -= 1;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Player is about to lose a life");
+                            if (playerLives <= 0)
+                            {
+                                playerLives = 0;
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Game Over");
+                            }
+                        }
+                    }
+                }
+                else if (playerShield <= 0)
+                {
+                    Console.WriteLine("Player is about to take " + damage + " points of damage");
+                    playerShield = 0;
+                    playerHealth = playerHealth - damage;
+                    if (playerHealth <= 0)
+                    {
+                        playerLives -= 1;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Player is about to lose a life");
+                        if (playerLives <= 0)
+                        {
+                            playerLives = 0;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Game Over");
+                        }
+                    }
+                }
+            }
+        }
+
+        static void RegenerateShield(int healAmount)
+        {
+            if(healAmount < 0)
+            {
+                Console.WriteLine("Player cannot regenerate a negative amount");
+            }
+
+            else
+            {
+                if (playerShield < 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Player is regenerating " + healAmount + " points of shields");
+                    playerShield = playerShield + healAmount;
+                    if (playerShield > 100)
+                    {
+                        playerShield = 100;
+                    }
+                }
+            }
         }
     }
 }
